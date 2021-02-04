5d915f117a754343fa9c1fb059900819fc94a3de
Dan Tcheche <dtcheche@gmail.com>
2021-02-04 15:33:43 -0300

    Add parse date time and add collection name to db_handler

--- utils.py
+++ utils.py
@@ -3,15 +3,21 @@ import telegram
 from db_connector.mongo_connector import MongoConnector
 
 
-def db_handler():
+def db_handler(collection_name):
     db_connector = MongoConnector()
     db_connector.create_database('your_social_life_bot')
-    col = db_connector.create_collection('event')
+    col = db_connector.create_collection(collection_name)
     return db_connector, col
 
 
-def parse_date(date_str):
+def parse_date_time(date_str):
     format_str = '%d/%m/%Y %H:%M'  # The format
+    date = datetime.datetime.strptime(date_str, format_str)
+    return date
+
+
+def parse_date(date_str):
+    format_str = '%d/%m/%Y'  # The format
     date = datetime.datetime.strptime(date_str, format_str)
     return date
 

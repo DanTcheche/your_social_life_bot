954d875c6249fa178f7e120d739d0f9c2619c2f3
Shay Elmualem <wowshay@Gmail.com>
2021-10-12 15:56:53 +0300

    feat: add next birthday handler (#4)
    * add next birthday handler
    
    * fix next birthday logic
    
    * remove unused
    
    * cr fixes

--- commands/birthdays/birthdays_handler.py
+++ commands/birthdays/birthdays_handler.py
@@ -2,8 +2,10 @@ from telegram.ext import Updater, CommandHandler
 
 from commands.birthdays.add_birthday import add_birthday
 from commands.birthdays.birthdays import birthdays
+from commands.birthdays.next_birthday import next_birthday
 
 
 def handle_birthdays(dp):
     dp.add_handler(CommandHandler('birthdays', birthdays))
     dp.add_handler(CommandHandler('add_birthday', add_birthday))
+    dp.add_handler(CommandHandler('next_birthday', next_birthday))
--- commands/birthdays/next_birthday.py
+++ commands/birthdays/next_birthday.py
@@ -1,3 +1,29 @@
+from utils import db_handler, send_message
+from datetime import datetime
+from commands.birthdays.utils import parse_birthday
 
-def next_birthday():
-    return
+def next_birthday(update, context):
+    chat_id = update.effective_chat.id
+    db_connector, birthdays_col = db_handler('birthday')
+
+    query = {"chat_id": chat_id}
+    next_birthday = db_connector.search_records(birthdays_col, query)
+
+    distance = 365
+    nearest_birthday = None
+    now = datetime.now()
+    for birthday in next_birthday:
+        difference_in_days = get_difference_between_dates_in_days(birthday['date'], now)
+        if difference_in_days < distance:
+            distance = difference_in_days
+            nearest_birthday = birthday
+    if not nearest_birthday:
+        return send_message(context, chat_id, "Please load birthdays first.")
+    message = f"Next birthday is in {difference_in_days} day(s): {parse_birthday(nearest_birthday)}"
+    return send_message(context, chat_id, message)
+
+def get_difference_between_dates_in_days(original_date, date_to_compare):
+    delta1 = datetime(date_to_compare.year, original_date.month, original_date.day)
+    delta2 = datetime(date_to_compare.year+1, original_date.month, original_date.day)
+    return ((delta1 if delta1 > date_to_compare else delta2) - date_to_compare).days
+
